0b6b103c682c77ec568b356a74f39ad757e4ca9e
Berox01 <45236095+Berox01@users.noreply.github.com>
2018-11-21 09:32:51 -0500

    Update quiz_tests.js

--- Les-Zones-Bleues/tests/quiz_tests.js
+++ Les-Zones-Bleues/tests/quiz_tests.js
@@ -1,3 +1,26 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
-});
+QUnit.test( "WHEN THEN", 
+    function( assert ) {
+         //arrange
+         totalPointage = 0;
+         
+         //act
+         ajouterPoint;
+         //assert
+        const EXPECTED_SCORE = 1;
+         assert.equal(EXPECTED_SCORE, totalPointage);
+    });
+
+QUnit.test( "WHEN_getPointage_THENscoreIsReturned", 
+    function( assert ) {
+    	//arrange
+    	totalPointage = 1;
+    	//act
+
+    	//assert
+         assert.ok( pointage(5) == 5, "Passed!" );
+    });
+
+Qunit.test("WHEN_")
+
+	var eScore = document.getElementById("totalPoints");
+	var currentScore = eScore.innerHTML
