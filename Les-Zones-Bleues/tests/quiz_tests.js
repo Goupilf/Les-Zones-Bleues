1952fad5f88c152fde9a22504d371db7a43a8cd8
Berox01 <45236095+Berox01@users.noreply.github.com>
2018-12-16 22:00:42 -0500

    Update quiz_tests.js

--- Les-Zones-Bleues/tests/quiz_tests.js
+++ Les-Zones-Bleues/tests/quiz_tests.js
@@ -1,26 +1,114 @@
-QUnit.test( "WHEN THEN", 
+QUnit.test( "WHEN_score is 0_THEN_ added score is 1_RETURNS_totalPointage",
     function( assert ) {
          //arrange
          totalPointage = 0;
          
          //act
-         ajouterPoint;
+         ajouterPoint();
          //assert
         const EXPECTED_SCORE = 1;
          assert.equal(EXPECTED_SCORE, totalPointage);
     });
+QUnit.test( "WHEN_score is 2_THEN_ added score is 3_RETURNS_totalPointage", 
+    function( assert ) {
+         //arrange
+         totalPointage = 2;
+         
+         //act
+         ajouterPoint();
+         //assert
+        const EXPECTED_SCORE = 3;
+         assert.equal(EXPECTED_SCORE, totalPointage);
+    });
+QUnit.test( "WHEN_score is 3_THEN_ added score is 4_RETURNS_totalPointage", 
+    function( assert ) {
+         //arrange
+         totalPointage = 3;
+         
+         //act
+         ajouterPoint();
+         //assert
+        const EXPECTED_SCORE = 4;
+         assert.equal(EXPECTED_SCORE, totalPointage);
+    });
 
-QUnit.test( "WHEN_getPointage_THENscoreIsReturned", 
+QUnit.test( "WHEN_getPointage_THEN_RETURNS_totalPointage", 
     function( assert ) {
     	//arrange
     	totalPointage = 1;
     	//act
-
+    	obtenirPointage(totalPointage);
     	//assert
-         assert.ok( pointage(5) == 5, "Passed!" );
+    	EXPECTED_POINTAGE = 1;
+         assert.equal( totalPointage, EXPECTED_POINTAGE );
     });
 
-Qunit.test("WHEN_")
 
-	var eScore = document.getElementById("totalPoints");
-	var currentScore = eScore.innerHTML
+QUnit.test("WHEN_noQuestionIs1_THEN_reponseUtilisateurIs1_RETURNS_noQuestion",
+	function(assert) {
+		//arrange
+		noQuestion = 1;
+		//act
+		obtenirChoix1 (noQuestion);
+		//assert
+		var EXPECTED_ANSWER = 1; 
+		assert.ok( noQuestion = EXPECTED_ANSWER );
+
+	});
+QUnit.test("WHEN_noQuestionIs2_THEN_reponseUtilisateurIs2_RETURNS_noQuestion",
+	function(assert) {
+		//arrange
+		noQuestion = 2;
+		//act
+		obtenirChoix2 (noQuestion);
+		//assert
+		var EXPECTED_ANSWER = 2; 
+		assert.ok( noQuestion = EXPECTED_ANSWER );
+
+	});
+
+QUnit.test("WHEN_noQuestionIs3_THEN_reponseUtilisateurIs3_RETURNS_noQuestion",
+	function(assert) {
+		//arrange
+		noQuestion = 3;
+		//act
+		obtenirChoix3 (noQuestion);
+		//assert
+		var EXPECTED_ANSWER = 3; 
+		assert.ok( noQuestion = EXPECTED_ANSWER );
+
+	});
+QUnit.test("WHEN_noQuestionIs4_THEN_reponseUtilisateurIs4_RETURNS_noQuestion",
+	function(assert) {
+		//arrange
+		noQuestion = 4;
+		//act
+		obtenirChoix4 (noQuestion);
+		//assert
+		var EXPECTED_ANSWER = 4; 
+		assert.ok( noQuestion = EXPECTED_ANSWER );
+
+	});
+
+	QUnit.test("WHEN_questionCouranteIs1_THEN_becomes2_RETURNS_questionCourante",
+	function(assert) {
+		//arrange
+		questionCourante = 1;
+		//act
+		majNoQuestionCourant (questionCourante);
+		//assert
+		var EXPECTED_ANSWER = 2; 
+		assert.ok( questionCourante = EXPECTED_ANSWER );
+
+	});
+	QUnit.test("WHEN_questionCouranteIs6_THEN_Game Over_RETURNS_questionCourante",
+	function(assert) {
+		//arrange
+		questionCourante = 6;
+		//act
+		majNoQuestionCourant(questionCourante);
+		//assert
+		var EXPECTED_ANSWER = true; 
+		assert.ok( finDeLaPartie = EXPECTED_ANSWER );
+
+	});
